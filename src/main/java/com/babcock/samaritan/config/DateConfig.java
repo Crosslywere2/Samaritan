58e8a56652674396f763cb0183fbc6ca4e22c4c2
Jude Ogboru <ogboruj@outlook.com>
2023-03-05 13:12:19 +0100

    Added date converter configuration

---
+++ src/main/java/com/babcock/samaritan/config/DateConfig.java
@@ -0,0 +1,25 @@
+package com.babcock.samaritan.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.format.datetime.DateFormatterRegistrar;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class DateConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    @Override
+    public FormattingConversionService mvcConversionService() {
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService(false);
+
+        DateFormatterRegistrar dateRegistrar = new DateFormatterRegistrar();
+        dateRegistrar.setFormatter(new DateFormatter("dd-MM-yyyy"));
+        dateRegistrar.registerFormatters(conversionService);
+
+        return conversionService;
+    }
+}
