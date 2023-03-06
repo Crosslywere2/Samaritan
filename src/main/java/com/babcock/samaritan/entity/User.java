d3d7ba6321a9c74f729ea2de9a9f5920eb762ab4
Jude Ogboru <ogboruj@outlook.com>
2023-03-06 14:15:53 +0100

    Added validation for email

--- src/main/java/com/babcock/samaritan/entity/User.java
+++ src/main/java/com/babcock/samaritan/entity/User.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Entity
@@ -32,6 +33,7 @@ public class User {
     @Enumerated(EnumType.ORDINAL)
     @Column(nullable = false)
     private Role role;
+    @Email
     @NotBlank(message = "Email required")
     @Column(nullable = false, unique = true)
     private String email;
