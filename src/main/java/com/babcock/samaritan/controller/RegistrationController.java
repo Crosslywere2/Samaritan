bfad05986c1bc081db383afe97d57b0d36855b41
Crosslywere2 <ogboruj@gmail.com>
2023-03-18 21:28:12 +0100

    Created route for registering Officers

--- src/main/java/com/babcock/samaritan/controller/RegistrationController.java
+++ src/main/java/com/babcock/samaritan/controller/RegistrationController.java
@@ -1,8 +1,12 @@
 package com.babcock.samaritan.controller;
 
+import com.babcock.samaritan.dto.AdminOfficerDTO;
+import com.babcock.samaritan.entity.Officer;
 import com.babcock.samaritan.entity.Student;
 import com.babcock.samaritan.entity.Token;
 import com.babcock.samaritan.error.DataAlreadyExistException;
+import com.babcock.samaritan.error.RequiredArgNotFoundException;
+import com.babcock.samaritan.service.OfficerService;
 import com.babcock.samaritan.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,14 +21,16 @@ import javax.validation.Valid;
 public class RegistrationController {
     @Autowired
     private StudentService studentService;
+    @Autowired
+    private OfficerService officerService;
 
     @PostMapping("/student")
     public Token registerStudent(@Valid @RequestBody Student student) throws DataAlreadyExistException {
         return studentService.registerStudent(student);
     }
 
-//    @PostMapping("/officer")
-//    public OfficerDTO registerOfficer(@Valid @RequestBody Officer officer) {
-//        return officerService.registerOfficer(officer);
-//    }
+    @PostMapping("/officer")
+    public AdminOfficerDTO registerOfficer(@Valid @RequestBody Officer officer) throws DataAlreadyExistException, RequiredArgNotFoundException {
+        return officerService.registerOfficer(officer);
+    }
 }
